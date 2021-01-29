7eb680f523bff11a20e775cf138823ee872dc8d6
eltonlazzarin <elton.lazzarin@outlook.com>
2021-01-29 19:36:02 -0300

    create empty movies list components

---
+++ src/components/EmptyMoviesList/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  span {
+    display: block;
+    text-align: center;
+    padding-top: 8.31rem;
+
+    h4 {
+      color: var(--white);
+      font-size: 1.3rem;
+      margin-top: 1rem;
+    }
+
+    p {
+      color: var(--gray);
+    }
+
+    img {
+      width: 8rem;
+      height: 4.3rem;
+    }
+  }
+`;
