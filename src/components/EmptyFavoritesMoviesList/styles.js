d3dc46537088dba58aef154616c8db45aaece289
eltonlazzarin <elton.lazzarin@outlook.com>
2021-01-29 19:34:36 -0300

    create empty favorites movies list components styles

---
+++ src/components/EmptyFavoritesMoviesList/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  span {
+    display: block;
+    text-align: center;
+    padding-top: 8.31rem;
+
+    h4 {
+      color: var(--white);
+      font-size: 1.3rem;
+      margin-top: 1rem;
+    }
+
+    a {
+      font-size: 1.2rem;
+      color: var(--gray);
+      text-decoration: none;
+    }
+
+    img {
+      width: 8rem;
+      height: 4.3rem;
+    }
+  }
+`;
