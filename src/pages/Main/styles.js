e183139dc8bdd840e9f47645877e7aa730e1c5f8
eltonlazzarin <elton.lazzarin@outlook.com>
2021-01-29 19:31:09 -0300

    create main page styles

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 66.25rem;
+  margin: 4.5rem auto;
+
+  font-size: 0.94rem;
+  font-weight: bold;
+
+  @media (max-width: 46rem) {
+    width: 90vw;
+  }
+`;
