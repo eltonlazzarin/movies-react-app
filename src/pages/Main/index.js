30ddfba119ab8005cde3b56db362882078c60904
Elton Lazzarin <53025782+eltonlazzarin@users.noreply.github.com>
2021-01-30 15:05:02 -0300

    fix buid error

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -48,7 +48,7 @@ export default function Main() {
   }, []);
 
   if (state.movies === undefined) {
-    //alert('Too many results. Please refine your search.');
+    // alert('Too many results. Please refine your search.');
     window.location.reload(false);
   }
 
